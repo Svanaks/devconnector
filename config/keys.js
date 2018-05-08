2d83fb49e49785c8e58ae19bd51c48898624f7d6
William Bloch <bloch.william@gmail.com>
2018-05-08 14:05:46 +0200

    change values keys for github

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://william:miam1407@ds157528.mlab.com:57528/devconnector",
-  secretOrKey: "secret"
+  mongoURI: "yourMongoDBPath",
+  secretOrKey: "yourKey"
 };
